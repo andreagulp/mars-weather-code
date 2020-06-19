57252b5a7cdfc4c9b266c0887808c33778b56576
andreagulp <andreagulp@users.noreply.github.com>
2020-06-19 21:59:10 +0200

    icon adjusted

--- src/components/TodayCard.js
+++ src/components/TodayCard.js
@@ -1,7 +1,10 @@
 import React, { Suspense } from "react";
-import { ReactComponent as Drop } from "../assets/drop.svg";
-import { ReactComponent as Wind } from "../assets/wind.svg";
+import { ReactComponent as Drop } from "../assets/drop2.svg";
+import { ReactComponent as Wind } from "../assets/wind2.svg";
+import { ReactComponent as Thermometer } from "../assets/thermometer.svg";
 import * as dayjs from "dayjs";
+
+//
 
 function TodayCard({ meteo, sol }) {
   console.log("TodayCard - METEO", meteo);
@@ -12,11 +15,10 @@ function TodayCard({ meteo, sol }) {
 
   return (
     <Suspense fallback={<div>...loading</div>}>
-      <div className="rounded-lg py-6 pl-8 pr-32 w-full bg-white opacity-100 ">
+      <div className="rounded-lg px-6 py-6 w-full opacity-100">
         <div className="mb-20">
           <h2 className="font-bold text-3xl leading-none pb-1">
-            {" "}
-            {`Mars Sol ${sol}`}{" "}
+            {`Mars Sol ${sol}`}
           </h2>
           <h3 className="leading-none pb-2 pl-1">
             {dayjs(meteo["Last_UTC"]).format("DD MMM YYYY")}
@@ -27,19 +29,19 @@ function TodayCard({ meteo, sol }) {
           </strong>
           <b className="text-2xl block font-bold">{meteo.Season}</b>
           <div className="grid grid-cols-2 grid-flow-col gap-4 my-2">
-            <div>Min</div>
+            <Thermometer className="fill-current  text-blue-600 inline-block w-6" />
             <div>{`${Math.floor(meteo["AT"]["mn"])}ºC`}</div>
           </div>
           <div className="grid grid-cols-2 grid-flow-col gap-4 my-2">
-            <div>Max</div>
+            <Thermometer className="fill-current text-orange-600 inline-block w-6" />
             <div>{`${Math.floor(meteo["AT"]["mx"])}ºC`}</div>
           </div>
           <div className="grid grid-cols-2 grid-flow-col gap-4 my-2">
-            <Drop className="w-4" />
+            <Drop className="fill-current text-blue-400 inline-block w-6" />
             <div>{`${Math.floor(meteo["PRE"]["av"])} Pa`}</div>
           </div>
-          <div className="grid grid-cols-2 grid-flow-col gap-4">
-            <Wind className="w-4" />
+          <div className="grid grid-cols-2 grid-flow-col gap-4 my-2">
+            <Wind className="fill-current text-purple-600 inline-block w-6 transform rotate-90" />
             <div>{`${Math.floor(meteo["HWS"]["av"])} s/m`}</div>
           </div>
         </div>
